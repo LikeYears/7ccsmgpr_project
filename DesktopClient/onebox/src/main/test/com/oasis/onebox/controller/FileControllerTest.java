edc32a770bb3676885b70785f7b666be1c39fb2f
YiboLiu1105 <46800822+YiboLiu1105@users.noreply.github.com>
2019-03-25 21:55:52 +0000

    test file

---
+++ DesktopClient/onebox/src/main/test/com/oasis/onebox/controller/FileControllerTest.java
@@ -0,0 +1,63 @@
+package com.oasis.onebox.controller;
+
+import com.oasis.onebox.interceptor.LoginInterceptor;
+import com.oasis.onebox.service.ShareFileService;
+import com.oasis.onebox.tool.FileTool;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class FileControllerTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void getFileLst() {
+    }
+
+    @Test
+    public void getFileList() {
+    }
+
+    @Test
+    public void fileUpload() {
+    }
+
+    @Test
+    public void fileDownload() {
+
+    }
+    @Test
+    public void delFile() {
+    }
+
+    @Test
+    public void moveFile() {
+    }
+
+    @Test
+    public void renameFile() {
+    }
+
+    @Test
+    public void createNewFile() {
+    }
+
+    @Test
+    public void createNewFile1() {
+    }
+
+    @Test
+    public void filePlay() {
+
+    }
+}
