fdaef91c16ad8afffa30c1d40f35b038c53421c0
YiboLiu1105 <46800822+YiboLiu1105@users.noreply.github.com>
2019-03-25 22:06:47 +0000

    test file

---
+++ DesktopClient/onebox/src/main/test/com/oasis/onebox/service/ShareFileServiceTest.java
@@ -0,0 +1,46 @@
+package com.oasis.onebox.service;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+
+import static org.junit.Assert.*;
+
+public class ShareFileServiceTest {
+    ShareFileService shareFileService;
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void generatePassword() {
+    }
+
+    @Test
+    public void addNewShareFile() {
+
+
+    }
+
+    @Test
+    public void searchAllShare() {
+    }
+
+    @Test
+    public void cancelShare() {
+    }
+
+    @Test
+    public void searchShareByID() {
+
+        Assert.assertEquals("","",ShareFileService.searchShareByID("22"));
+    }
+}
