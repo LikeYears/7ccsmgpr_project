f5355e16f68b8e6493c6afcb63008d4f77eca4a8
Yankai Chen <chenykdavid@gmail.com>
2019-03-26 17:29:27 +0000

    Update Register and Welcome Unit Test

--- MobileClient/onebox/app/src/test/java/com/oasis/onebox/RegisterActivityTest.java
+++ MobileClient/onebox/app/src/test/java/com/oasis/onebox/RegisterActivityTest.java
@@ -1,20 +1,43 @@
 package com.oasis.onebox;
 
+
 import org.junit.Test;
+
+import java.util.concurrent.TimeUnit;
+
+import okhttp3.OkHttpClient;
 
 import static org.junit.Assert.*;
 
 public class RegisterActivityTest {
-
+    private String url = "169.254.146.86";
+    //valid user
+    private String vun = "123";
+    private String vup = "111";
+    //invalid user
+    private String iun = "1233";
+    private String iup = null;
+    private String response = "succeed";
     @Test
     public void initOkHttp() {
+        OkHttpClient oHC = new OkHttpClient.Builder()
+                .connectTimeout(10000L, TimeUnit.MILLISECONDS)
+                .readTimeout(10000L, TimeUnit.MILLISECONDS)
+                .build();
+        assertNotNull(oHC);
     }
 
     @Test
     public void getPublicKey() {
+
+        assertNotNull(response);
+        assertNull(iup);
     }
 
     @Test
     public void postUser() {
+        String error = "fail to post";
+        assertNotNull(response);
+        //assertNull(error);
     }
 }
