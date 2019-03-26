c2b443bda9508fd193e46d5450a4e326d431fdbf
Yankai Chen <chenykdavid@gmail.com>
2019-03-26 16:15:12 +0000

    Register and Welcome Unit Test

---
+++ MobileClient/onebox/app/src/test/java/com/oasis/onebox/RegisterActivityTest.java
@@ -0,0 +1,20 @@
+package com.oasis.onebox;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class RegisterActivityTest {
+
+    @Test
+    public void initOkHttp() {
+    }
+
+    @Test
+    public void getPublicKey() {
+    }
+
+    @Test
+    public void postUser() {
+    }
+}
---
+++ MobileClient/onebox/app/src/test/java/com/oasis/onebox/WelcomeActivityTest.java
@@ -0,0 +1,20 @@
+package com.oasis.onebox;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class WelcomeActivityTest {
+
+    @Test
+    public void onCreate() {
+    }
+
+    @Test
+    public void getPublicKey() {
+    }
+
+    @Test
+    public void postUser() {
+    }
+}
