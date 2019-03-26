6fa53345246064ca837fb7c0db9cef8887d01a5c
Zhihao Zhu <zhihao.zhu@kcl.ac.uk>
2019-03-26 22:15:39 +0800

    style.css

---
+++ DesktopClient/onebox/src/main/java/com/oasis/onebox/controller/LoginController.java
@@ -0,0 +1,56 @@
+package com.oasis.onebox.controller;
+
+
+import com.oasis.onebox.tool.RSA;
+import com.oasis.onebox.tool.ResultShowing;
+import com.oasis.onebox.tool.StringTool;
+import com.oasis.onebox.entity.User;
+import com.oasis.onebox.exception.CustomException;
+import com.oasis.onebox.interceptor.LoginInterceptor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+@RequestMapping("/login")
+public class LoginController {
+
+    //send public key to front end
+    @RequestMapping(method = RequestMethod.GET)
+    @ResponseBody
+    public ResultShowing getpubkey() {
+        return new ResultShowing("", RSA.getInstance().getPubKey());
+    }
+
+    @RequestMapping(method = RequestMethod.POST, produces = "application/json")
+    @ResponseBody
+    public ResultShowing auth(HttpServletRequest request) throws CustomException {
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+        if (StringTool.isNullOrEmpty(username) || StringTool.isNullOrEmpty(password)) {
+            throw new CustomException(401, "username or password is null", null);
+        }
+        username = RSA.getInstance().decryptByPriKey(username);
+        password = RSA.getInstance().decryptByPriKey(password);
+        User u = User.loginAuth(username, password);
+        if (u != null) {
+            String token = u.createToken(request);
+            return new ResultShowing("login success", token);
+        }
+
+        throw new CustomException(401, "username or password not correct", null);
+    }
+
+
+    @RequestMapping(value = "/logout", method = RequestMethod.DELETE, produces = "application/json")
+    @ResponseBody
+    public ResultShowing logout(@RequestAttribute(LoginInterceptor.LOGIN_USER) User loginUser) {
+        loginUser.setToken(null);
+        loginUser.setExpirationtime(-1);
+        return new ResultShowing("ok", null);
+    }
+}
---
+++ DesktopClient/onebox/target/onebox/WEB-INF/classes/com/oasis/onebox/tool/EncodeTool.java
@@ -0,0 +1,70 @@
+package com.oasis.onebox.tool;
+
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.text.SimpleDateFormat;
+import java.util.Base64;
+
+public class EncodeTool {
+    public static final SimpleDateFormat FORMMATTER = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+
+    public static final byte[] decoderBASE64(String key) {
+        return Base64.getDecoder().decode(key);
+    }
+
+    public static final String encoderBASE64(byte[] key) {
+        return Base64.getEncoder().encodeToString(key);
+    }
+
+    public static final byte[] decoderURLBASE64(String key) {
+        return Base64.getUrlDecoder().decode(key);
+    }
+
+    public static final String encoderURLBASE64(byte[] key) {
+        return Base64.getUrlEncoder().encodeToString(key);
+    }
+
+    public static final String bytesToHexString(byte[] bArray) {
+        StringBuffer sb = new StringBuffer(bArray.length);
+        String sTemp;
+        for (int i = 0; i < bArray.length; i++) {
+            sTemp = Integer.toHexString(0xFF & bArray[i]);
+            if (sTemp.length() < 2)
+                sb.append(0);
+            sb.append(sTemp.toUpperCase());
+        }
+        return sb.toString();
+    }
+
+    public static byte[] hexStringToByte(String hex) {
+        int len = (hex.length() / 2);
+        byte[] result = new byte[len];
+        char[] achar = hex.toCharArray();
+        for (int i = 0; i < len; i++) {
+            int pos = i * 2;
+            result[i] = (byte) (toByte(achar[pos]) << 4 | toByte(achar[pos + 1]));
+        }
+        return result;
+    }
+
+    private static int toByte(char c) {
+        byte b = (byte) "0123456789ABCDEF".indexOf(c);
+        return b;
+    }
+
+    public static String MD5(String str, int i) {
+        try {
+            i = i == 0 ? 10 : i;
+            MessageDigest md = MessageDigest.getInstance("MD5");
+            int x = 0;
+            while (x < i) {
+                md.update(str.getBytes());
+                str = new BigInteger(1, md.digest()).toString(16);
+                x++;
+            }
+        } catch (Exception e) {
+
+        }
+        return str;
+    }
+}
